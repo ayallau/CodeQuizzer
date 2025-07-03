0029afca6346b467cf521502901753a305a0f585
ayallau <ayallau@gmail.com>
2025-07-03 04:05:31 +0300

    First Structure

--- Client/CodeQuizzer/src/app/app.module.ts
+++ Client/CodeQuizzer/src/app/app.module.ts
@@ -3,10 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { WelcomeComponent } from './pages/welcome/welcome.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { TopicComponent } from './pages/topic/topic.component';
+import { ProfileComponent } from './pages/profile/profile.component';
+import { QuizComponent } from './pages/quiz/quiz.component';
+import { MainLayoutComponent } from './layouts/main-layout/main-layout.component';
+import { HeaderComponent } from './layouts/header/header.component';
+import { FooterComponent } from './layouts/footer/footer.component';
+import { LoginComponent } from './components/login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WelcomeComponent,
+    DashboardComponent,
+    TopicComponent,
+    ProfileComponent,
+    QuizComponent,
+    MainLayoutComponent,
+    HeaderComponent,
+    FooterComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
